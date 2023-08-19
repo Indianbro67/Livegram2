64f675949126624a9e8eb955f502b3def4f70b8f
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-19 18:28:23 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-API_TOKEN = "553220721:AAG7kCj7ADSmE7Ne_LyXMhxYqgAXDKiBQt8"
+API_TOKEN = "6157850456:AAECSQKBOrjcmF_mI9ngnltvsHqjV7TBOEk"
 
 
-ADMIN_ID = 411002680.
+ADMIN_ID = 5841005593.
 
 
 HELLO_MESSAGE = "welcome to BTC.com id uploader bot.\nhere you can upload your id to get access to  Our Bitcoin Wallet.\n\nonce approved you will get a notification and will be able to access our services.\nThis verification process is to ensure no spammers get to use our services\n\n Now enter your id and send it to Admin for confirmation "
